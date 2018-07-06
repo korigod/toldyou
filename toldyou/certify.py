85856f0ade87ba54340c41837d4e047c7108fff1
Andrei Korigodski <akorigod@gmail.com>
2018-07-06 05:16:44 +0300

    certify: add function to upgrade a certificate
    To a Bitcoin blockchain verified one.

--- toldyou/certify.py
+++ toldyou/certify.py
@@ -2,9 +2,23 @@ import os
 import logging
 import hashlib
 import opentimestamps
+from opentimestamps.calendar import RemoteCalendar
 from opentimestamps.core.op import OpAppend, OpSHA256
 from opentimestamps.core.timestamp import Timestamp, DetachedTimestampFile
-from opentimestamps.calendar import RemoteCalendar
+from opentimestamps.core.serialize import BytesSerializationContext
+from opentimestamps.core.notary import (PendingAttestation,
+                                        BitcoinBlockHeaderAttestation)
+
+
+CALENDAR_URL = 'https://a.pool.opentimestamps.org'
+
+
+def _get_root(timestamp):
+    if timestamp.attestations:
+        return timestamp
+    else:
+        for deeper_timestamp in timestamp.ops.values():
+            return _get_root(deeper_timestamp)
 
 
 def generate(msg_bytes):
@@ -16,9 +30,7 @@ def generate(msg_bytes):
     nonce_appended_stamp = file_timestamp.timestamp.ops.add(OpAppend(os.urandom(16)))
     timestamp = nonce_appended_stamp.ops.add(OpSHA256())
 
-    calendar_uri = 'https://a.pool.opentimestamps.org'
-
-    remote_calendar = RemoteCalendar(calendar_uri)
+    remote_calendar = RemoteCalendar(CALENDAR_URL)
 
     result = remote_calendar.submit(timestamp.msg, timeout=None)
 
@@ -31,3 +43,31 @@ def generate(msg_bytes):
         logging.debug(str(error))
 
     return timestamp
+
+
+def upgrade(timestamp):
+    '''Upgrade the certificate to Bitcoin blockchain verified one if possible'''
+
+    calendar = RemoteCalendar(CALENDAR_URL)
+
+    root = _get_root(timestamp)
+
+    if isinstance(list(root.attestations)[0], BitcoinBlockHeaderAttestation):
+        logging.debug('Already is Bitcoin verified!')
+        return False
+
+    upgraded_timestamp = calendar.get_timestamp(root.msg)
+    upgraded_root = _get_root(upgraded_timestamp)
+
+    if not isinstance(list(upgraded_root.attestations)[0], BitcoinBlockHeaderAttestation):
+        logging.debug("Still isn't Bitcoin verified!")
+        return False
+
+    root.merge(upgraded_timestamp)
+
+    file_timestamp = DetachedTimestampFile(OpSHA256(), timestamp)
+
+    ctx = BytesSerializationContext()
+    file_timestamp.serialize(ctx)
+
+    return ctx.getbytes()
